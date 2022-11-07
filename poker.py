de5fbe24f81214df61ab97129a1768bf56eb77cb
Lucas Peres <lucasperes95@gmail.com>
2022-11-07 03:01:34 -0300

    moved tests to pytest module and added allmax func to handle ties

--- poker.py
+++ poker.py
@@ -82,48 +82,9 @@ def hand_rank(hand):
     else:
         return (0, ranks)
 
+def allmax(iterable, key=(lambda k: k)):
+    return [i for i in iterable if key(i) == key(max(iterable, key=key))]
+
 def poker(hands):
-    "Return the best hand: poker([hand,...]) => hand"
-    return max(hands, key=hand_rank)
-
-## tests ##
-def test():
-    "Test cases"
-    sf = ['6C', '7C', '8C', '9C', 'TC']
-    fk = ['9D', '9H', '9S', '9C', '7D']
-    fh = ['TD', 'TC', 'TH', '7C', '7D']
-    tp = ['5S', '5D', '9H', '9C', '6S']
-    s1 = ['AS', '2S', '3S', '4S', '5C'] # A-5 straight
-    s2 = ['2C', '3C', '4C', '5S', '6S'] # 2-6 straight
-    ah = ['AS', '2S', '3S', '4S', '6C'] # A high
-    sh = ['2S', '3S', '4S', '6C', '7D'] # 7 high
-
-    assert poker([s1, s2, ah, sh]) == s2
-    
-    fkranks = card_ranks(fk)
-    tpranks = card_ranks(tp)
-    assert kind(4, fkranks) == 9
-    assert kind(3, fkranks) == None
-    assert kind(2, fkranks) == None
-    assert kind(1, fkranks) == 7
-    assert two_pair(fkranks) == None
-    assert two_pair(tpranks) == (9, 5)
-    
-    assert straight([9, 8, 7, 6, 5]) == True
-    assert straight([9, 8, 8, 6, 5]) == False
-    assert flush(sf) == True
-    assert flush(fk) == False
-    assert card_ranks(sf) == [10, 9, 8, 7, 6]
-    assert card_ranks(fk) == [9, 9, 9, 9, 7]
-    assert card_ranks(fh) == [10, 10, 10, 7, 7]
-    assert poker([fk]) == fk
-    assert poker([fh, fh]) == fh
-    assert poker([fk, fh]) == fk
-    assert poker([sf, fk, fh]) == sf
-    assert poker([sf] + 99 * [fh]) == sf
-    assert hand_rank(sf) == (8, 10)
-    assert hand_rank(fk) == (7, 9, 7)
-    assert hand_rank(fh) == (6, 10, 7)
-    return "tests pass"
-print(test())
-##########
+    "Return the best hands: poker([hand,...]) => [hand,...]"
+    return allmax(hands, key=hand_rank)
