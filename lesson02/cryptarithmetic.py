62c9374a43851cae0dd475640828c44c24ee7052
Lucas Peres <lucasperes95@gmail.com>
2022-11-11 03:55:01 -0300

    added solve and sketch for fill_in

--- lesson02/cryptarithmetic.py
+++ lesson02/cryptarithmetic.py
@@ -7,3 +7,9 @@ def valid(formula):
         return not re.search(r'\b0[0-9]', formula) and eval(formula) is True
     except ArithmeticError:
         return False
+
+def solve(formula):
+    return next(assign for assign in fill_in(formula) if valid(assign))
+
+def fill_in(formula):
+    pass
