ab578b0e2f6e96fcde20896d864918e351f4f0ad
Lucas Peres <lucasperes95@gmail.com>
2022-11-13 04:13:27 -0300

    added naive soln to subpalindrome

--- lesson02/ps02/subpalindrome.py
+++ lesson02/ps02/subpalindrome.py
@@ -4,25 +4,16 @@ def palindrome(text):
     # O(len(text))
     if not text: 
         return True
-    return text[0] == text[len(text)-1] if palindrome(text[1:len(text)-1]) else False
+    return text[0].upper() == text[len(text)-1].upper() if palindrome(text[1:len(text)-1]) else False
 
 def longest_subpalindrome_slice(text):
     print(text)
-    if not text:
-        return 0, 0
-    elif len(text) == 1:
+    n = len(text)
+    if n == 0:
         return 0, 0
     else:
-        mid = len(text) // 2
-        i, j = longest_subpalindrome_slice(text[:mid])
-        k, l = longest_subpalindrome_slice(text[mid:])
-        # text[:mid][-1] == text[:mid][0]
-        # text[:mid][-2] == text[:mid][1]
-        # text[:mid][-3] == text[:mid][2]
-        # and so on
-        x, y = -1, 0
-        while text[:mid][x] == text[mid:][y]:
-            x -= 1
-            y += 1
-        x = mid - x
-        return max([(i,j), (k,l), (x,y)], key=lambda tup: tup[1]-tup[0])
+        i, j = longest_subpalindrome_slice(text[:n-1])
+        for k in range(n):
+            if palindrome(text[k:n]) and j - i < n - k:
+                return k, n
+        return i, j
