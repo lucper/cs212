45e84abc3aef76a3afa8e44cd7268e0a16276817
Lucas Peres <lucasperes95@gmail.com>
2022-11-13 04:50:24 -0300

    added sketch of faster soln

--- lesson02/ps02/subpalindrome.py
+++ lesson02/ps02/subpalindrome.py
@@ -7,7 +7,6 @@ def palindrome(text):
     return text[0].upper() == text[len(text)-1].upper() if palindrome(text[1:len(text)-1]) else False
 
 def longest_subpalindrome_slice(text):
-    print(text)
     n = len(text)
     if n == 0:
         return 0, 0
@@ -17,3 +16,23 @@ def longest_subpalindrome_slice(text):
             if palindrome(text[k:n]) and j - i < n - k:
                 return k, n
         return i, j
+
+def faster_longest_subpalindrome_slice(text):
+    n = len(text)
+    if n == 0:
+        return 0, 0
+    elif n == 1:
+        return 0, 1
+    else:
+        mid = n // 2
+        i, j = faster_longest_subpalindrome_slice(text[:mid])
+        k, l = faster_longest_subpalindrome_slice(text[mid:])
+        # overlap case
+        p, q = mid-1, mid
+        while text[p] == text[q] and q < n and p > -1:
+            p -= 1
+            q += 1
+        if text[p] == text[q]:
+            return max([(i, j), (k+len(text[:mid]), l+len(text[:mid])), (p, q+len(text[:mid]))], key=lambda tup: tup[1]-tup[0])
+        else:
+            return max([(i, j), (k+len(text[:mid]), l+len(text[:mid]))], key=lambda tup: tup[1]-tup[0])
