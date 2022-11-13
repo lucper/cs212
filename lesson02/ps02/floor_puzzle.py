9f980383ae9927f6e8d2fc55a2b7ea30d73e2e8c
Lucas Peres <lucasperes95@gmail.com>
2022-11-13 02:31:17 -0300

    added mapping for apartments and helper funcs sketches

---
+++ lesson02/ps02/floor_puzzle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# Hopper, Kay, Liskov, Perlis, and Ritchie live on 
+# different floors of a five-floor apartment building. 
+#
+# Hopper does not live on the top floor. 
+# Kay does not live on the bottom floor. 
+# Liskov does not live on either the top or the bottom floor. 
+# Perlis lives on a higher floor than does Kay. 
+# Ritchie does not live on a floor adjacent to Liskov's. 
+# Liskov does not live on a floor adjacent to Kay's. 
+# 
+# Where does everyone live?  
+
+import itertools
+
+apartments = bottom, _, _, _, top = [0, 1, 2, 3, 4]
+
+def higher(x, y):
+    pass
+
+def adjacent(x, y):
+    pass
+
+def floor_puzzle():
+    # Your code here
+    return [Hopper, Kay, Liskov, Perlis, Ritchie]
