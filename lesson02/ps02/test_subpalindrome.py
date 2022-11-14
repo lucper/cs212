ca2e01419b1d218f9181ba9dbc4b79a6c5813e68
Lucas Peres <lucasperes95@gmail.com>
2022-11-14 16:25:23 -0300

    added tests of longest subpalindrome

--- lesson02/ps02/test_subpalindrome.py
+++ lesson02/ps02/test_subpalindrome.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-from subpalindrome import longest_subpalindrome_slice
+from subpalindrome import *
 
 def test():
-    assert longest_subpalindrome_slice('racecar') == (0, 7)
-    assert longest_subpalindrome_slice('Racecar') == (0, 7)
-    assert longest_subpalindrome_slice('RacecarX') == (0, 7)
-    assert longest_subpalindrome_slice('Race carr') == (7, 9)
-    assert longest_subpalindrome_slice('') == (0, 0)
-    assert longest_subpalindrome_slice('something rac e car going') == (8,21)
-    assert longest_subpalindrome_slice('xxxxx') == (0, 5)
-    assert longest_subpalindrome_slice('Mad am I ma dam.') == (0, 15)
+    assert faster_longest_palindrome_slice('racecar') == (0, 7)
+    assert faster_longest_palindrome_slice('Racecar') == (0, 7)
+    assert faster_longest_palindrome_slice('RacecarX') == (0, 7)
+    assert faster_longest_palindrome_slice('Race carr') == (7, 9)
+    assert faster_longest_palindrome_slice('') == (0, 0)
+    assert faster_longest_palindrome_slice('something rac e car going') == (8,21)
+    assert faster_longest_palindrome_slice('xxxxx') == (0, 5)
+    assert faster_longest_palindrome_slice('Mad am I ma dam.') == (0, 15)
