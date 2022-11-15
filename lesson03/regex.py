ff28cc89bdbab11ca40a655a2a9450a2a645b964
Lucas Peres <lucasperes95@gmail.com>
2022-11-14 22:46:19 -0300

    added my implementation of regex processor (still in very early stage)

---
+++ lesson03/regex.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# Let T be a sequence of chaarcters ("" included).
+# We have the following patterns P:
+# ""    empty string
+# a     first occurrence of character 'a'
+# .     first occurrence of a single character, e.g. (".", "abc") => True
+# ^a    line beginning with character 'a', e.g. ("^a", "abc") => True
+# a?    zero or one occurrence of 'a' in T
+# a*    zero or more occurrences of 'a' in T
+# a$    line ending with character 'a', e.g. ("a$", "abc") => False
+# ab    first occurrence of characters 'a' and 'b' concatenated
+
+def match_here(pattern, text):
+    if not pattern:
+        return True
+    if text and (pattern[0] == text[0] or pattern[0] == '.'):
+        return match_here(pattern[1:], text[1:])
+    return False
+
+def search(pattern, text):
+    if not text:
+        return not pattern
+    if match_here(pattern, text):
+        return True
+    return search(pattern, text[1:])
---
+++ lesson03/test_regex.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+from regex import *
+
+def test_search_empty_string_vs_emppy_string():
+    assert search("", "") == True
+
+def test_search_empty_string_vs_anything():
+    assert search("", "bru") == True
+
+def test_search_anyhing_vs_empty_string():
+    assert search("^a", "") == False
+    assert search("a$", "") == False
+    assert search("$", "") == False
+    assert search(".", "") == False
+    assert search("a", "") == False
+    assert search("a?", "") == False
+    assert search("a*", "") == False
+
+def test_search_one_char_vs_anything():
+    assert search("a", "ab") == True
+    assert search("c", "ab") == False
+
+def test_search_two_char_vs_anything():
+    assert search("ab", "cabc") == True
+    assert search("cc", "abab") == False
+
+def test_matchhere_empty_vs_anything():
+    assert match_here("", "") == True
+    assert match_here("", "bru") == True
+
+def test_matchhere_anything_vs_empty():
+    assert match_here("^a", "") == False
+    assert match_here("a$", "") == False
+    assert match_here("$", "") == False
+    assert match_here(".", "") == False
+    assert match_here("a", "") == False
+    assert match_here("a?", "") == False
+    assert match_here("a*", "") == False
+
+def test_matchhere_dot():
+    assert match_here(".", "") == False
+    assert match_here(".", "a") == True
+    assert match_here(".a", "a") == False
+    assert match_here(".a", "ca") == True
+    assert match_here(".a.b", "yaxb") == True
