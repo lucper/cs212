d970e079250c1efe5c6b8a1aa6f15f3e99277f2c
Lucas Peres <lucasperes95@gmail.com>
2022-11-21 00:26:21 -0300

    added regex compiler (incomplete)

---
+++ lesson03/regex_compiler.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+## API for operators
+
+#dot = ('dot',)
+#eol = ('eol',)
+
+def lit(string):
+    """ Literal """
+    return lambda text: set([text[len(string):]]) if text.startswith(string) else None
+
+def seq(lit1, lit2):
+    """ Concatenation """
+    pass
+
+def alt(lit1, lit2):
+    """ | operator, e.g. 'a|b' matches either 'a' or 'b' """
+    pass
+
+def star(lit):
+    """ * operator """
+    pass
+
+def plus(lit):
+    """ + operator """
+    pass
+
+def opt(lit):
+    """ ? operator """
+    pass
+
+def oneof(chars):
+    pass
+
+## 
+
+#def matchset(pattern, text):
+#    op, x, y = components(pattern)
+#    if op == 'lit':
+#        return set([text[len(x):]]) if text.startswith(x) else frozenset()
+#    elif op == 'seq':
+#        return set(t2 for t1 in matchset(x, text) for t2 in matchset(y, t1))
+#    elif op == 'alt':
+#        return matchset(x, text) | matchset(y, text)
+#    elif op == 'dot':
+#        return set([text[1:]]) if text else None
+#    elif op == 'oneof':
+#        return set([text[1:]]) if text.startswith(x) else None
+#    elif op == 'eol':
+#        return set(['']) if not text else None
+#    elif op == 'star':
+#        # !!!
+#        return set([text]) | set(t2 for t1 in matchset(x, text) for t2 in matchset(pattern, t1) if t1 != text)
+#    else:
+#        raise ValueError(f'unknown pattern: {pattern}')
+
+def match(pattern, text):
+    """Match pattern against start of text; return longest if found or None otherwise"""
+    remainders = pattern(text)
+    if remainders:
+        shortest = min(remainders, key=len)
+        # !!!
+        return text[:len(text)-len(shortest)]
+
+def search(pattern, text):
+    for i in range(len(text)):
+        if m := match(pattern, text[i:]):
+            return m
